76da58ebc6c3146eed96711538d0c5181975f79c
Amjad <sammanamjad86@gmail.com>
2020-05-05 08:58:58 +0200

    finish

--- movies-share/src/App.js
+++ movies-share/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import './App.css'
 
 
 
@@ -21,6 +22,14 @@ class App extends Component {
   submitHandler = (e) => {
     e.preventDefault()
     console.log(this.state)
+    axios.post('https://post-a-form.herokuapp.com/api/movies', this.state)
+      .then(res => res.data)
+      .then(res => {
+        alert('Your Favorite Movie is added !');
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
   render() {
     const { title, poster, comment } = this.state
@@ -28,13 +37,16 @@ class App extends Component {
       <div className="App">
         <form onSubmit={this.submitHandler}>
           <div>
-            <input type='text' name='title' value={title} onChange={this.changeHandler}></input>
+            <label for="fname">Movie Title :</label>
+            <input type='text' name='title' value={title} onChange={this.changeHandler} />
           </div>
           <div>
-            <input type='text' name='poster' value={poster} onChange={this.changeHandler}></input>
+            <label for="fname">Movie Poster :</label>
+            <input type='text' name='poster' value={poster} onChange={this.changeHandler} />
           </div>
           <div>
-            <input type='text' name='comment' value={comment} onChange={this.changeHandler}></input>
+            <label for="fname">Add a Comment :</label>
+            <input type='text' name='comment' value={comment} onChange={this.changeHandler} />
           </div>
           <button type="submit">Share</button>
         </form>
